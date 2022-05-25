4be55e92acc88b79e8cf6733e34faf7058d392db
tycoonkabuzi <kabuzitycoon@gmail.com>
2022-05-25 15:55:58 +0200

    Adding the 3 lines effect syncronized with the slides

--- javascript/script.js
+++ javascript/script.js
@@ -1,12 +1,11 @@
 // declaration des variables interessante.
-const cons=document.getElementsByClassName("carousel__title");
 const slideElement=document.getElementsByClassName("carousel__element");
 const main=document.querySelector(".carousel__main");
 const slide=document.querySelector(".carousel__content");
 const nextBtn=document.querySelector(".carousel__direction--right");
 const prevBtn=document.querySelector(".carousel__direction--left");
 const navlines=document.querySelector(".carousel__point-direction");
-const lines=document.querySelector(".carousel__point");
+const lines=document.querySelectorAll(".carousel__point");
 
 
 // creating the mecanism to move the slide.
@@ -15,6 +14,7 @@ function moveSlide(currentElement,targetElement,targetElementIndex){
    currentElement.classList.remove("current");
    targetElement.classList.add("current");
 }
+
 // creating a convertor function which will be taking in parameter the name of the class to be changed in a proper array
 function converter(toBeConverted){
    const arrayElmenent=[];
@@ -23,27 +23,44 @@ function converter(toBeConverted){
   }
   return arrayElmenent;
 }
+
+function updateDots(targetElement,currentDot){
+   targetElement.classList.add("carousel__point--current");
+   currentDot.classList.remove("carousel__point--current");
+   if (targetElement==null){
+      targetElement.classList.add("current");
+   }
+}
 // next button click listener created
-nextBtn.addEventListener("click", function(){
-   const currentElement=main.querySelector(".current");
-   const nextElement=currentElement.nextElementSibling;
-   const nextTargetIndex=converter(slideElement).findIndex(slide=> slide===nextElement);
-   console.log(nextTargetIndex);
+nextBtn.addEventListener("click", function(e){
+   const currentDot=navlines.querySelector(".carousel__point--current");
+   const nextDot=currentDot.nextElementSibling;
+   const currentElement=main.querySelector(".current");// find the current elment on which we are
+   const nextElement=currentElement.nextElementSibling;// create the next element simbling
+   const nextTargetIndex=converter(slideElement).findIndex(slide=> slide===nextElement);// find a target index 
    moveSlide(currentElement,nextElement,nextTargetIndex);
+   updateDots(nextDot,currentDot);
+   
 });
-prevBtn.addEventListener("click", function(){
+prevBtn.addEventListener("click", function(e){
+   const currentDot=navlines.querySelector(".carousel__point--current");
+   const prevDot=currentDot.previousElementSibling;
    const currentElement=main.querySelector(".current");
    const prevElement=currentElement.previousElementSibling;
-   const prevTargetIndex=converter(slideElement).findIndex(slide=> slide===prevElement);
+   const prevTargetIndex=/* this is an array*/converter(slideElement).findIndex(slide=> slide===prevElement);
    moveSlide(currentElement,prevElement,prevTargetIndex);
+   updateDots(prevDot,currentDot);
+   
+  
    
 });
 navlines.addEventListener("click",function(e){
+   const currentElement=main.querySelector(".current");
    const currentDot=navlines.querySelector(".carousel__point--current");
    const targetElement=e.target.closest('span');
-   const prevdot=currentDot.previousElementSibling;
-   console.log(prevdot);
-   targetElement.classList.add("carousel__point--current");
-   currentDot.classList.remove("carousel__point--current");
-   prevBtn.classList.remove('carousel__point--current');
+   const targetIndex=converter(lines).findIndex(dot=> dot===targetElement);//Find the index of the line
+   const targetSlide=converter(slideElement)[targetIndex];
+   moveSlide(currentElement,targetSlide,targetIndex);
+   //slide.style.transform='translatex(-'+100*targetIndex+'%'+')';// move the element by targetting the index of the line element.
+   updateDots(targetElement,currentDot);
 });
